a25e5506d31b43900ccc2495c6b29add763a2f6d
Michael Bottom <mlbmike@gmail.com>
2020-11-10 01:02:13 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'; // test comment
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home';
 import Project from './components/Project';
