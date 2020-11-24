ee0a8acc7597150ecb42156037b2eecac53ad340
t0rna <73195801+t0rna@users.noreply.github.com>
2020-11-24 12:49:47 -0600

    Update db.js

--- server/db.js
+++ server/db.js
@@ -11,7 +11,7 @@ const getFeedback = () => {
   return new Promise(function(resolve, reject) {
     pool.query('SELECT * FROM Feedback ORDER BY HonestPoints', (error, results) => {
       if (error)
-	  {
+      {
         reject(error);
       }
       resolve(results.rows);
@@ -24,7 +24,7 @@ const createFeedback = (body) => {
     const {FeedbackID, subject, txt, PID} = body;
     pool.query('INSERT INTO Feedback (FeedbackID, subject, text, PID) VALUES ($1, $2, $3, $4) RETURNING *', [FeedbackID, subject, txt, PID], (error, results) => {
       if (error) 
-	  {
+      {
         reject(error);
       }
       resolve("A new feedback has been added: ${results.rows[0]}");
@@ -32,11 +32,24 @@ const createFeedback = (body) => {
   });
 };
 
+const updateFeedback = (body) => {
+  return new Promise(function(resolve, reject) {
+    const {FID, Points} = body;
+    pool.query('UPDATE Feedback SET HonestPoints = Points WHERE FeedbackID = FID', (error, results) => {
+      if (error)
+      {
+	 reject(error);
+      }
+      resolve(results.rows);
+    });
+  });
+});
+
 const getProject = () => {
   return new Promise(function(resolve, reject) {
     pool.query('SELECT * FROM Project ORDER BY ProjectID', (error, results) => {
       if (error)
-	  {
+      {
         reject(error);
       }
       resolve(results.rows);
@@ -49,7 +62,7 @@ const createProject = (body) => {
     const {ProjectID, AdminID, TeamID, TasksID, Name} = body;
     pool.query('INSERT INTO Projects (ProjectID, AdminID, TeamID, TasksID, Name) VALUES ($1, $2, $3, $4, $5) RETURNING *', [ProjectID, AdminID, TeamID, TasksID, Name], (error, results) => {
       if (error)
-	  {
+      {
         reject(error);
       }
       resolve("A new project has been added: ${results.rows[0]}");
@@ -61,7 +74,7 @@ const getUsers = () => {
   return new Promise(function(resolve, reject) {
     pool.query('SELECT * FROM Users ORDER BY ID', (error, results) => {
       if (error)
-	  {
+      {
         reject(error);
       }
       resolve(results.rows);
@@ -69,12 +82,25 @@ const getUsers = () => {
   });
 };
 
+const updateUsers = (body) => {
+  return new Promise(function(resolve, reject) {
+    const {UID, pwd} = body;
+    pool.query('UPDATE Users SET password = pwd WHERE ID = UID', (error, results) => {
+      if (error)
+      {
+	 reject(error);
+      }
+      resolve(results.rows);
+    });
+  });
+});
+
 const createUser = (body) => {
   return new Promise(function(resolve, reject) {
     const {ID, username, pwd} = body;
     pool.query('INSERT INTO Users (ID, username, password) VALUES ($1, $2, $3) RETURNING *', [ID, username, pwd], (error, results) => {
       if (error) 
-	  {
+      {
         reject(error);
       }
       resolve("A new user has been added: ${results.rows[0]}");
@@ -86,7 +112,7 @@ const getTasks = () => {
   return new Promise(function(resolve, reject) {
     pool.query('SELECT * FROM Tasks ORDER BY ProjectID', (error, results) => {
       if (error)
-	  {
+      {
         reject(error);
       }
       resolve(results.rows);
@@ -99,7 +125,7 @@ const createTask = (body) => {
     const {TaskID, TaskName, ProjectID} = body;
     pool.query('INSERT INTO Users (TaskID, TaskName, ProjectID) VALUES ($1, $2, $3) RETURNING *', [TaskID, TaskName, ProjectID], (error, results) => {
       if (error) 
-	  {
+      {
         reject(error);
       }
       resolve("A new task has been added: ${results.rows[0]}");
@@ -112,7 +138,7 @@ const deleteTask = () => {
     const id = parseInt(request.params.id);
     pool.query('DELETE FROM Tasks WHERE TaskID = $1', [id], (error, results) => {
       if (error) 
-	  {
+      {
         reject(error);
       }
       resolve("Task deleted with ID: ${id}");
@@ -124,7 +150,7 @@ const getTeams = () => {
   return new Promise(function(resolve, reject) {
     pool.query('SELECT * FROM Teams ORDER BY TeamID', (error, results) => {
       if (error)
-	  {
+      {
         reject(error);
       }
       resolve(results.rows);
@@ -137,7 +163,7 @@ const createTeam = (body) => {
     const {TeamID, UserID} = body;
     pool.query('INSERT INTO Teams(TeamID, UserID) VALUES ($1, $2) RETURNING *', [TeamID, UserID], (error, results) => {
       if (error) 
-	  {
+      {
         reject(error);
       }
       resolve("A new team has been added: ${results.rows[0]}");
@@ -150,7 +176,7 @@ const deleteTeam = () => {
     const id = parseInt(request.params.id);
     pool.query('DELETE FROM Teams WHERE TeamID = $1', [id], (error, results) => {
       if (error) 
-	  {
+      {
         reject(error);
       }
       resolve("Team deleted with ID: ${id}");
@@ -161,9 +187,11 @@ const deleteTeam = () => {
 module.exports = {
   getFeedback,
   createFeedback,
+  updateFeedback,
   getProject,
   createProject,
   getUsers,
+  updateUsers,
   createUser,
   getTasks,
   createTask,
